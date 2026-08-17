412a4036af77620fff5c62d878da2ab0bf2b111c
Brites <antoniombrites2716@gmail.com>
2026-08-17 20:10:06 -0300

    Entendendo as equações

---
+++ 2-semestre/programming-and-algorithms/equacao01.py
@@ -0,0 +1,10 @@
+# Um numero somado ao seu triplo resulta em 48
+#x + 3x = 48
+#4x = 48
+#x = 48/4
+#x = 12
+
+x = 12
+resultado = x + (3 * x)
+
+print(f"O número é {x} seu triplo é {3*x}. Somados o resultado é {resultado}.")
